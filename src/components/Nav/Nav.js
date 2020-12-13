c55def9349cf21fecc7d1c82ddab0524ce1f2d38
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-12-13 20:17:37 +0600

    upload file

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import logo from '../../images/logo.svg'
 
 const Nav = () => {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        
     );
 };
 
