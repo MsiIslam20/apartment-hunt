b9f5a56ebfe05a554b951e32580d2c515e531ec0
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-12-13 21:07:00 +0600

    upload file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <section className="rent-header">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-7 mx-auto">
+                        <h1>FIND YOUR HOUSE RENT</h1>
+                        <div className="d-flex">
+                            <input className="form-control" type="text" placeholder="Search......."/>
+                            <button className="btn btn-primary default-btn">Find Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
