431ef0aa753b4fcfd3ab4142c3b2370c97554cfc
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-12-13 21:44:49 +0600

    upload file

---
+++ src/components/HotelList/HotelList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HotelList.css';
+
+const HotelList = () => {
+    return (
+        <section className="hotel-list-wrapper">
+            
+        </section>
+    );
+};
+
+export default HotelList;
