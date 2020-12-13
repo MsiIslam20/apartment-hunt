0f3d7ae407660dacca3d256d2965bb7d81a9fdad
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-12-13 22:45:45 +0600

    upload file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import HotelList from '../HotelList/HotelList';
 import Nav from '../Nav/Nav';
+import Service from '../Services/Service';
 
 const Home = () => {
     return (
@@ -9,6 +11,8 @@ const Home = () => {
             <Nav></Nav>
             <Header></Header>
             <HotelList></HotelList>
+            <Service></Service>
+            <Footer></Footer>
         </>
     );
 };
