73de053360f3538881428e6d8077fdd48ab516e2
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-12-13 19:44:12 +0600

    upload file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav/Nav';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div>
+    <>
       <Nav></Nav>
-    </div>
+      <Header></Header>
+    </>
   );
 }
 
