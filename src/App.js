9cc574658744d8578bc60ab350305ef6a1dfd251
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-12-14 11:04:44 +0600

    update File

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import DetailsHome from './components/HomeMain/DetailsHome/DetailsHome';
 import Home from './components/HomeMain/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home></Home>
-      <DetailsHome></DetailsHome>
-    </>
+    <Router>
+      <Switch>
+        <Route path="/rentDetails">
+          <DetailsHome></DetailsHome>
+        </Route>
+        <Route exact path="/">
+            <Home />
+        </Route>        
+      </Switch>
+    </Router>
   );
 }
 
