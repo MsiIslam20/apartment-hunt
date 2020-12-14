3e934b8478c3ca65922bbea2e8e24de33ec17d1d
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-12-14 11:17:51 +0600

    update File

--- src/components/HomeMain/Details/Details.js
+++ src/components/HomeMain/Details/Details.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Details = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="rent-header rent_home">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-7 mx-auto">
+                        <h1>Apartment</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
