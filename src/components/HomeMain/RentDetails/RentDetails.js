bf4c786545a14cc07db81d160cf3f0efd21c59ec
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-12-14 11:56:29 +0600

    update File

---
+++ src/components/HomeMain/RentDetails/RentDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const RentDetails = () => {
+    return (
+        <section className="rent-wrapper">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default RentDetails;
